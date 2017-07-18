761dc5747d9f2869cf4ad8eacf9808bffc20df69
ytbryan <ytbryan@gmail.com>
2017-07-18 22:47:47 +0800

    pull to refresh

--- js/kitchen-sink.js
+++ js/kitchen-sink.js
@@ -6,6 +6,34 @@ var myApp = new Framework7({
 // Expose Internal DOM library
 var $$ = Dom7;
 
+myApp.onPageInit('index', function (page) {
+    // Dummy Content
+    var songs = ['Yellow Submarine', 'Don\'t Stop Me Now', 'Billie Jean', 'Californication'];
+    var authors = ['Beatles', 'Queen', 'Michael Jackson', 'Red Hot Chili Peppers'];
+    // Pull to refresh content
+    var ptrContent = $$(page.container).find('.pull-to-refresh-content');
+    // Add 'refresh' listener on it
+    ptrContent.on('refresh', function (e) {
+        // Emulate 2s loading
+        setTimeout(function () {
+            var picURL = 'http://lorempixel.com/88/88/abstract/' + Math.round(Math.random() * 10);
+            var song = songs[Math.floor(Math.random() * songs.length)];
+            var author = authors[Math.floor(Math.random() * authors.length)];
+            var linkHTML = '<li class="item-content">' +
+                                '<div class="item-media"><img src="' + picURL + '" width="44"/></div>' +
+                                '<div class="item-inner">' +
+                                    '<div class="item-title-row">' +
+                                        '<div class="item-title">' + song + '</div>' +
+                                    '</div>' +
+                                    '<div class="item-subtitle">' + author + '</div>' +
+                                '</div>' +
+                            '</li>';
+            ptrContent.find('ul').prepend(linkHTML);
+            // When loading done, we need to "close" it
+            myApp.pullToRefreshDone();
+        }, 2000);
+    });
+});
 
 function time() {
     var now = new moment();
