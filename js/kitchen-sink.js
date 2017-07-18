2db7aa6a9e027506ca2ee67a5577098ea77f8c72
ytbryan <ytbryan@gmail.com>
2017-07-18 19:57:19 +0800

    reduce js further

--- js/kitchen-sink.js
+++ js/kitchen-sink.js
@@ -41,13 +41,6 @@ var mainView = myApp.addView('.view-main', {
     // Enable Dynamic Navbar for this view
     dynamicNavbar: true,
 });
-// Add another view, which is in right panel
-var rightView = myApp.addView('.view-right', {
-    // Enable Dynamic Navbar for this view
-    dynamicNavbar: true,
-    name: 'right'
-});
-
 
 myApp.onPageInit('show', function (page) {
 
@@ -107,143 +100,143 @@ $$(document).on('ajaxComplete', function (e) {
     myApp.hideIndicator();
 });
 
-// Callbacks for specific pages when it initialized
-/* ===== Modals Page events  ===== */
-myApp.onPageInit('modals', function (page) {
+// // Callbacks for specific pages when it initialized
+// /* ===== Modals Page events  ===== */
+// myApp.onPageInit('modals', function (page) {
 
-    // $$('.toggle-sortable').on('click', function () {
-    //     if ($(this).text() == "Done")
-    //         $(this).text("Edit")
-    //     else
-    //         $(this).text("Done");
-    //     myApp.sortableToggle('.sortable');
-    // });
-
-
-    $$('.demo-alert').on('click', function () {
-        myApp.alert('Hello!');
-    });
-    $$('.demo-confirm').on('click', function () {
-        myApp.confirm('Are you feel good today?', function () {
-            myApp.alert('Great!');
-        });
-    });
-    $$('.demo-prompt').on('click', function () {
-        myApp.prompt('What is your name?', function (data) {
-            // @data contains input value
-            myApp.confirm('Are you sure that your name is ' + data + '?', function () {
-                myApp.alert('Ok, your name is ' + data + ' ;)');
-            });
-        });
-    });
-    $$('.demo-login').on('click', function () {
-        myApp.modalLogin('Enter your username and password', function (username, password) {
-            myApp.alert('Thank you! Username: ' + username + ', password: ' + password);
-        });
-    });
-    $$('.demo-password').on('click', function () {
-        myApp.modalPassword('Enter your password', function (password) {
-            myApp.alert('Thank you! Password: ' + password);
-        });
-    });
-    $$('.demo-modals-stack').on('click', function () {
-        // Open 5 alerts
-        myApp.alert('Alert 1');
-        myApp.alert('Alert 2');
-        myApp.alert('Alert 3');
-        myApp.alert('Alert 4');
-        myApp.alert('Alert 5');
-    });
-    $$('.demo-picker-modal').on('click', function () {
-        myApp.pickerModal('.picker-modal-demo');
-    });
-});
-
-/* ===== Preloader Page events ===== */
-myApp.onPageInit('preloader', function (page) {
-
-    $$('.demo-indicator').on('click', function () {
-        myApp.showIndicator();
-        setTimeout(function () {
-            myApp.hideIndicator();
-        }, 2000);
-    });
-    $$('.demo-preloader').on('click', function () {
-        myApp.showPreloader();
-        setTimeout(function () {
-            myApp.hidePreloader();
-        }, 2000);
-    });
-    $$('.demo-preloader-custom').on('click', function () {
-        myApp.showPreloader('My text...');
-        setTimeout(function () {
-            myApp.hidePreloader();
-        }, 2000);
-    });
-});
-
-/* ===== Swipe to delete events callback demo ===== */
-myApp.onPageInit('swipe-delete', function (page) {
-    $$('.demo-remove-callback').on('deleted', function () {
-        myApp.alert('Thanks, item removed!');
-    });
-});
-myApp.onPageInit('swipe-delete media-lists', function (page) {
-    $$('.demo-reply').on('click', function () {
-        myApp.alert('Reply');
-    });
-    $$('.demo-mark').on('click', function () {
-        myApp.alert('Mark');
-    });
-    $$('.demo-forward').on('click', function () {
-        myApp.alert('Forward');
-    });
-});
+//     // $$('.toggle-sortable').on('click', function () {
+//     //     if ($(this).text() == "Done")
+//     //         $(this).text("Edit")
+//     //     else
+//     //         $(this).text("Done");
+//     //     myApp.sortableToggle('.sortable');
+//     // });
 
 
-/* ===== Action sheet, we use it on few pages ===== */
-myApp.onPageInit('swipe-delete modals media-lists', function (page) {
-    var actionSheetButtons = [
-        // First buttons group
-        [
-            // Group Label
-            {
-                text: 'Here comes some optional description or warning for actions below',
-                label: true
-            },
-            // First button
-            {
-                text: 'Alert',
-                onClick: function () {
-                    myApp.alert('He Hoou!');
-                }
-            },
-            // Another red button
-            {
-                text: 'Nice Red Button ',
-                color: 'red',
-                onClick: function () {
-                    myApp.alert('You have clicked red button!');
-                }
-            },
-        ],
-        // Second group
-        [
-            {
-                text: 'Cancel',
-                bold: true
-            }
-        ]
-    ];
-    $$('.demo-actions').on('click', function (e) {
-        myApp.actions(actionSheetButtons);
-    });
-    $$('.demo-actions-popover').on('click', function (e) {
-        // We need to pass additional target parameter (this) for popover
-        myApp.actions(this, actionSheetButtons);
-    });
+//     $$('.demo-alert').on('click', function () {
+//         myApp.alert('Hello!');
+//     });
+//     $$('.demo-confirm').on('click', function () {
+//         myApp.confirm('Are you feel good today?', function () {
+//             myApp.alert('Great!');
+//         });
+//     });
+//     $$('.demo-prompt').on('click', function () {
+//         myApp.prompt('What is your name?', function (data) {
+//             // @data contains input value
+//             myApp.confirm('Are you sure that your name is ' + data + '?', function () {
+//                 myApp.alert('Ok, your name is ' + data + ' ;)');
+//             });
+//         });
+//     });
+//     $$('.demo-login').on('click', function () {
+//         myApp.modalLogin('Enter your username and password', function (username, password) {
+//             myApp.alert('Thank you! Username: ' + username + ', password: ' + password);
+//         });
+//     });
+//     $$('.demo-password').on('click', function () {
+//         myApp.modalPassword('Enter your password', function (password) {
+//             myApp.alert('Thank you! Password: ' + password);
+//         });
+//     });
+//     $$('.demo-modals-stack').on('click', function () {
+//         // Open 5 alerts
+//         myApp.alert('Alert 1');
+//         myApp.alert('Alert 2');
+//         myApp.alert('Alert 3');
+//         myApp.alert('Alert 4');
+//         myApp.alert('Alert 5');
+//     });
+//     $$('.demo-picker-modal').on('click', function () {
+//         myApp.pickerModal('.picker-modal-demo');
+//     });
+// });
 
-});
+// /* ===== Preloader Page events ===== */
+// myApp.onPageInit('preloader', function (page) {
+
+//     $$('.demo-indicator').on('click', function () {
+//         myApp.showIndicator();
+//         setTimeout(function () {
+//             myApp.hideIndicator();
+//         }, 2000);
+//     });
+//     $$('.demo-preloader').on('click', function () {
+//         myApp.showPreloader();
+//         setTimeout(function () {
+//             myApp.hidePreloader();
+//         }, 2000);
+//     });
+//     $$('.demo-preloader-custom').on('click', function () {
+//         myApp.showPreloader('My text...');
+//         setTimeout(function () {
+//             myApp.hidePreloader();
+//         }, 2000);
+//     });
+// });
+
+// /* ===== Swipe to delete events callback demo ===== */
+// myApp.onPageInit('swipe-delete', function (page) {
+//     $$('.demo-remove-callback').on('deleted', function () {
+//         myApp.alert('Thanks, item removed!');
+//     });
+// });
+// myApp.onPageInit('swipe-delete media-lists', function (page) {
+//     $$('.demo-reply').on('click', function () {
+//         myApp.alert('Reply');
+//     });
+//     $$('.demo-mark').on('click', function () {
+//         myApp.alert('Mark');
+//     });
+//     $$('.demo-forward').on('click', function () {
+//         myApp.alert('Forward');
+//     });
+// });
+
+
+// /* ===== Action sheet, we use it on few pages ===== */
+// myApp.onPageInit('swipe-delete modals media-lists', function (page) {
+//     var actionSheetButtons = [
+//         // First buttons group
+//         [
+//             // Group Label
+//             {
+//                 text: 'Here comes some optional description or warning for actions below',
+//                 label: true
+//             },
+//             // First button
+//             {
+//                 text: 'Alert',
+//                 onClick: function () {
+//                     myApp.alert('He Hoou!');
+//                 }
+//             },
+//             // Another red button
+//             {
+//                 text: 'Nice Red Button ',
+//                 color: 'red',
+//                 onClick: function () {
+//                     myApp.alert('You have clicked red button!');
+//                 }
+//             },
+//         ],
+//         // Second group
+//         [
+//             {
+//                 text: 'Cancel',
+//                 bold: true
+//             }
+//         ]
+//     ];
+//     $$('.demo-actions').on('click', function (e) {
+//         myApp.actions(actionSheetButtons);
+//     });
+//     $$('.demo-actions-popover').on('click', function (e) {
+//         // We need to pass additional target parameter (this) for popover
+//         myApp.actions(this, actionSheetButtons);
+//     });
+
+// });
 
 
 
